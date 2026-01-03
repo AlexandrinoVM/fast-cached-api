a9568fa990911ac6457be123b8ff8aaedb33d2af
AlexandrinoVM <vinicius.moraes2@outlook.com>
2026-01-03 16:42:11 -0300

    addint the itemRespository and the delete route

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -1,6 +1,7 @@
 import { Response,Request } from "express"
 import pool from "../db/pg_client.js"
 import redis, { connectRedis } from "../db/redis_client.js"
+import { ItemRespository } from "../repositories/ItemRepository.js";
 export interface ItemInterFaceDTO{
     name:string,
     id:number
@@ -22,14 +23,13 @@ export interface Product {
 export class ItemService {
     static async CreateItem(data:Product){
         const {id,title,price,description} = data 
-        const User = await pool.query(`INSERT into itens (name,id_item,price,description) 
-            VALUES ($1,$2,$3,$4) RETURNING id,name,id_item,price,description`,[title,id,price,description]) 
-        return User.rows[0]
+        const item = await ItemRespository.Create(id,title,price,description) 
+        return item
     }
 
     static async ListItems(){
-        const result =await pool.query("SELECT id,name,id_item from itens") 
-        return result.rows
+        const result = await ItemRespository.List()  
+        return result
     }
 
     static async listById(id:number){
@@ -43,10 +43,9 @@ export class ItemService {
         }        
 
         //try get from the database 
-        const dbiten = await pool.query("SELECT id,name,id_item from itens where id_item = ($1)",[id])
-        if(dbiten.rowCount != 0){
+        const item=await ItemRespository.ListById(id) 
+        if(item){
             console.log("returned by db")
-            const item = dbiten.rows[0]
             await redis.set(cacheKey,JSON.stringify(item),{EX:300})
             return item
         }
@@ -60,16 +59,20 @@ export class ItemService {
         const product:Product = await getItemApi.json() 
 
         //set new iten to the cache and in the database
-        await this.CreateItem(product) 
-        await redis.set(cacheKey,JSON.stringify(product),{EX:300})
+        const created = await ItemRespository.Create(product.id,
+        product.title,
+        product.price,
+        product.description
+        ) 
+        await redis.set(cacheKey,JSON.stringify(created),{EX:300})
 
         console.log('returned by external api')
-        return product
+        return created 
     }
     
     static async deleteItems(id:number){
-        const result = await pool.query('DELETE FROM itens where id = $1',[id]) 
-        if(result.rowCount != 0){
+        const result =await ItemRespository.Delete(id) 
+        if(result){
             redis.del(`item:${id}`)
         }
         return
